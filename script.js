9b6878cd290a11a0d2a95bf476d120e1b4f4bbaa
prakashGit2000 <prakash4snu@gmail.com>
2026-05-06 23:02:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
+// script.js
+
 const CONFIG = {
 
-  SHEET_ID: "https://script.google.com/macros/s/AKfycbyCJWvSfe1N0tzvNHjs6zYLeGT0u0hoosap4KY4pimlxpWIiCCEf2Bv_sPMdjMZJT3SjA/exec",
-
-  JDOODLE_CLIENT_ID: "9491bccf73d58219a773cb4b36d7432a",
-
-  JDOODLE_CLIENT_SECRET: "ab5635380d4487d3a48432cf97441ba083d0c06fb836660ec5ce6d6b70675e40"
+  API_URL:
+  "https://script.google.com/macros/s/AKfycbyCJWvSfe1N0tzvNHjs6zYLeGT0u0hoosap4KY4pimlxpWIiCCEf2Bv_sPMdjMZJT3SjA/exec"
 
 };
 
 let editor;
 
-let currentQuestion = null;
+let questions = [];
+
+let currentIndex = 0;
+
+let emailGlobal = "";
 
 let timerInterval;
 
-let timeLeft = 0;
+let currentQuestionId = "";
 
-window.onload = function () {
+window.onload = function(){
 
   editor = ace.edit("editor");
 
@@ -38,26 +41,24 @@ window.onload = function () {
 
 };
 
-function startTest() {
+function startTest(){
 
-  const email = document.getElementById("email").value;
+  emailGlobal = document.getElementById("email").value;
 
-  if(email.trim() === ""){
+  if(emailGlobal.trim() === ""){
 
     alert("Enter Email");
 
     return;
   }
 
-  fetch(CONFIG.SHEET_ID, {
+  fetch(CONFIG.API_URL, {
 
     method:"POST",
 
     body:JSON.stringify({
 
-      action:"getQuestion",
-
-      email:email
+      action:"getQuestions"
 
     })
 
@@ -65,39 +66,93 @@ function startTest() {
   .then(res => res.json())
   .then(data => {
 
-    currentQuestion = data;
+    questions = data;
 
-    document.getElementById("questionTitle").innerText = data.title || "Coding Question";
+    currentIndex = 0;
 
-    document.getElementById("questionText").innerText = data.problem || "";
+    loadQuestion();
 
-    let sampleHTML = "";
+  });
 
-    if(data.samples){
+}
 
-      data.samples.forEach(s => {
+function loadQuestion(){
 
-        sampleHTML += `
-        <div class="sample-box">
-          <b>Input:</b><br>${s.input}<br><br>
-          <b>Output:</b><br>${s.output}
-        </div>
-        `;
+  if(currentIndex >= questions.length){
 
-      });
+    alert("Test Completed");
 
-    }
+    return;
+  }
 
-    document.getElementById("samples").innerHTML = sampleHTML;
+  currentQuestionId = questions[currentIndex];
 
-    startTimer(data.time || 300);
+  fetch(CONFIG.API_URL, {
+
+    method:"POST",
+
+    body:JSON.stringify({
+
+      action:"start",
+
+      email:emailGlobal,
+
+      qid:currentQuestionId
+
+    })
 
   })
-  .catch(err => {
+  .then(res => res.json())
+  .then(data => {
 
-    console.log(err);
+    if(!data.allowed){
 
-    alert("Backend Error");
+      alert("Not Allowed / Already Submitted");
+
+      currentIndex++;
+
+      loadQuestion();
+
+      return;
+    }
+
+    document.getElementById("questionTitle").innerText =
+      data.problem;
+
+    document.getElementById("questionNumber").innerText =
+      "Question: " + currentQuestionId;
+
+    document.getElementById("questionText").innerText =
+      data.problem;
+
+    let html = "";
+
+    data.samples.forEach(s => {
+
+      html += `
+      <div class="sample-box">
+
+        <b>Input:</b>
+        <br>
+        ${s.input}
+
+        <br><br>
+
+        <b>Output:</b>
+        <br>
+        ${s.output}
+
+      </div>
+      `;
+
+    });
+
+    document.getElementById("samples").innerHTML = html;
+
+    startTimer(data.time * 60);
+
+    document.getElementById("output").innerText =
+      "Question Loaded Successfully";
 
   });
 
@@ -107,21 +162,19 @@ function startTimer(seconds){
 
   clearInterval(timerInterval);
 
-  timeLeft = seconds;
+  let timeLeft = seconds;
 
-  updateTimer();
+  updateTimer(timeLeft);
 
   timerInterval = setInterval(() => {
 
     timeLeft--;
 
-    updateTimer();
+    updateTimer(timeLeft);
 
     if(timeLeft <= 0){
 
       clearInterval(timerInterval);
-
-      alert("Time Up");
 
       submitCode();
     }
@@ -130,17 +183,18 @@ function startTimer(seconds){
 
 }
 
-function updateTimer(){
+function updateTimer(seconds){
 
-  let min = Math.floor(timeLeft / 60);
+  let min = Math.floor(seconds / 60);
 
-  let sec = timeLeft % 60;
+  let sec = seconds % 60;
 
   min = min < 10 ? "0" + min : min;
 
   sec = sec < 10 ? "0" + sec : sec;
 
-  document.getElementById("timer").innerText = `${min}:${sec}`;
+  document.getElementById("timer").innerText =
+    `${min}:${sec}`;
 
 }
 
@@ -166,47 +220,42 @@ function getAceMode(language){
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  const lang = document.getElementById("language");
+  document.getElementById("language")
+  .addEventListener("change", function(){
 
-  if(lang){
+    editor.session.setMode(
+      getAceMode(this.value)
+    );
 
-    lang.addEventListener("change", function(){
-
-      editor.session.setMode(getAceMode(this.value));
-
-    });
-
-  }
+  });
 
 });
 
 function runCode(){
 
-  const language = document.getElementById("language").value;
+  const language =
+    document.getElementById("language").value;
 
   const code = editor.getValue();
 
-  document.getElementById("output").innerText = "Compiling...";
+  document.getElementById("output").innerText =
+    "Compiling...";
 
-  fetch("https://api.jdoodle.com/v1/execute", {
+  fetch(CONFIG.API_URL, {
 
     method:"POST",
 
-    headers:{
-      "Content-Type":"application/json"
-    },
-
     body:JSON.stringify({
 
-      clientId:CONFIG.JDOODLE_CLIENT_ID,
+      action:"submit",
 
-      clientSecret:CONFIG.JDOODLE_CLIENT_SECRET,
+      email:"compile@temp.com",
 
-      script:code,
+      qid:currentQuestionId,
 
-      language:language,
+      code:code,
 
-      versionIndex:getLanguageVersion(language)
+      language:language
 
     })
 
@@ -215,12 +264,13 @@ function runCode(){
   .then(data => {
 
     document.getElementById("output").innerText =
-      data.output || data.error || "No Output";
+      `Passed: ${data.marks}/${data.total}`;
 
   })
   .catch(err => {
 
-    document.getElementById("output").innerText = err;
+    document.getElementById("output").innerText =
+      err;
 
   });
 
@@ -228,13 +278,15 @@ function runCode(){
 
 function submitCode(){
 
-  const email = document.getElementById("email").value;
-
-  const language = document.getElementById("language").value;
+  const language =
+    document.getElementById("language").value;
 
   const code = editor.getValue();
 
-  fetch(CONFIG.SHEET_ID, {
+  document.getElementById("output").innerText =
+    "Submitting...";
+
+  fetch(CONFIG.API_URL, {
 
     method:"POST",
 
@@ -242,11 +294,13 @@ function submitCode(){
 
       action:"submit",
 
-      email:email,
+      email:emailGlobal,
 
-      language:language,
+      qid:currentQuestionId,
 
-      code:code
+      code:code,
+
+      language:language
 
     })
 
@@ -254,13 +308,21 @@ function submitCode(){
   .then(res => res.json())
   .then(data => {
 
-    alert("Code Submitted Successfully");
+    document.getElementById("output").innerText =
+      `Final Score: ${data.marks}/${data.total}`;
 
-  })
-  .catch(err => {
-
-    console.log(err);
+    alert(
+      `Marks: ${data.marks}/${data.total}`
+    );
 
   });
+
+}
+
+function nextQuestion(){
+
+  currentIndex++;
+
+  loadQuestion();
 
 }
